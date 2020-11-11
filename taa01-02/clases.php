cef5f950ddf06e02ddea186f9af89693e008fa79
mkvaltierra <mvaltierra@birt.eus>
2020-11-11 22:19:50 +0100

    First commit project taa01-02

---
+++ taa01-02/clases.php
@@ -0,0 +1,19 @@
+<?php 
+
+    /* Main Class */
+    class Vehicle {
+
+        //Atributos
+        private $matricula;
+        private $modelo;
+        private $potencia;
+
+        //Constructor
+        public function __constructor(){
+
+        }
+    }
+
+
+
+
---
+++ taa01-02/index.php
@@ -0,0 +1,14 @@
+<?php include clases.php ?>
+<?php include datos.php ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarea Aprendizaje  01 - 02</title>
+</head>
+<body>
+    
+</body>
+</html>
